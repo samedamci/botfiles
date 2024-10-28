4812b31e1efa79b89036f35d1f4f08e83a2fa5b2
Wiktor Zykubek <dev@wzykubek.xyz>
2024-10-28 21:55:38 +0100

    Add nvim-like statusline to yazi
    Not perfect yet but simmilar at least.

---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,66 @@
+require("yatline"):setup({
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
+
+	style_a = { fg = "black", bg_mode = {
+		normal = "#ebbcba",
+		select = "#c4a7e7",
+		un_set = "#c4a7e7",
+	} },
+	style_b = { bg = "#2d2b38", fg = "#e0def4" },
+	style_c = { bg = "#201e2b", fg = "#706e7d" },
+
+	selected = { icon = "󰻭", fg = "yellow" },
+	copied = { icon = "", fg = "green" },
+	cut = { icon = "", fg = "red" },
+
+	total = { icon = "󰮍", fg = "yellow" },
+	succ = { icon = "", fg = "green" },
+	fail = { icon = "", fg = "red" },
+	found = { icon = "󰮕", fg = "blue" },
+	processed = { icon = "󰐍", fg = "green" },
+
+	show_background = true,
+
+	display_header_line = true,
+	display_status_line = true,
+
+	header_line = {
+		left = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+	},
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_name" },
+			},
+			section_c = {
+				{ type = "coloreds", custom = false, name = "count" },
+				{ type = "coloreds", custom = false, name = "task_states" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+})
